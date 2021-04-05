8ee97b5dda8e83bb5f6c266eb5a293c728352cc6
shanmukhkagal <kagalshanmukh@gmail.com>
2021-04-05 12:17:55 -0700

    [snakeladderUC2 ]add: programs

---
+++ corejava/snakeladderUC2.java
@@ -0,0 +1,20 @@
+class snakeladderUC2
+{
+     public static void main(String[] args)
+     {
+           int position = 0;
+           int dice_number = 0;
+           int random = (int)Math.floor(Math.random() * 10)%6;
+           if(random!=0)
+           {
+
+                System.out.println("game starts with random position"+random);
+           }
+
+
+     }
+
+
+}
+
+
