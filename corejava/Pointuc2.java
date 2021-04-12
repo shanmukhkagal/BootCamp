0b6ab89e3c2678436bdcd1cf7f2501101f8a8f33
shanmukhkagal <kagalshanmukh@gmail.com>
2021-04-12 07:21:44 -0700

    [Pointuc2 ]add: welcome

---
+++ corejava/Pointuc2.java
@@ -0,0 +1,13 @@
+public class Pointuc2 {
+	double x;
+	double y;
+	double a;
+	double b;
+
+	public  Pointuc2(double x, double y, double a, double b) {
+        this.x=x;
+        this.y=y;
+        this.a=a;
+        this.b=b;
+     }
+}
