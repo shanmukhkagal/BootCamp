35d4558091e06e9c3ddd47c79419adea0188d571
shanmukhkagal <kagalshanmukh@gmail.com>
2021-04-12 07:27:51 -0700

    [Pointuc3 ]add: welcome

---
+++ corejava/Pointuc3.java
@@ -0,0 +1,13 @@
+public class Pointuc3 {
+	double x;
+	double y;
+	double a;
+	double b;
+
+	public  Pointuc3(double x, double y, double a, double b) {
+        this.x=x;
+        this.y=y;
+        this.a=a;
+        this.b=b;
+    }
+}
