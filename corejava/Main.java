0dee9aa121acdc15344abc5c9f5b97faa941a47a
shanmukhkagal <kagalshanmukh@gmail.com>
2021-04-12 07:15:21 -0700

    [Main ]add: welcome

---
+++ corejava/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+	public static void main(String[] args)
+	{
+		Point p=new Point(56, 8);
+		
+		Point p1=new Point(6, 85);
+       
+		Line l1=new Line(p,p1);
+		
+		System.out.println(l1.getLength());
+	}
+}
