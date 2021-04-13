4cd27cad4470848b5933de6ae51fe6153e58ef6e
shanmukhkagal <kagalshanmukh@gmail.com>
2021-04-13 12:28:31 -0700

    [ TotalWagesInMonth.java ]add: Welcome To Employee Wage Computation

---
+++ corejava/TotalWagesInMonth.java
@@ -0,0 +1,28 @@
+public class TotalWagesInMonth {
+	 public static final int IS_FULL_TIME = 0;
+	  public static final int EMP_RATE_PER_HOUR = 20;
+	  public static final int NUM_OF_WORKING_DAYS = 20;
+	  public static final int MAX_HRS_IN_MONTH = 100;
+	  public static int salary = 0;
+	  
+	  public static int computeWage() {
+		  int empHrs = 0, totalEmpHrs = 0 , totalWorkingDays = 0;
+		  while(totalEmpHrs <= 100 && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+			  totalWorkingDays++;
+			  int empCheck = (int) Math.floor(Math.random() * 10 ) % 1;
+			  switch(empCheck) {
+			   case IS_FULL_TIME:
+			    	 empHrs = 5;
+			    	 break;
+		       }
+			  totalEmpHrs += empHrs;
+			  System.out.println("Days:"+totalWorkingDays+" total Emp Hr: "+totalEmpHrs);
+           }
+		   int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+		   System.out.println("Emp wage is :"+totalEmpWage);
+		   return totalEmpWage;
+	  }
+	  public static void main(String[] args) {
+		  computeWage();
+	  }
+}O
