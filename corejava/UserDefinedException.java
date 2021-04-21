22be89f8ebb6ecdc3397f7b69cd8c28a67927177
shanmukhkagal <kagalshanmukh@gmail.com>
2021-04-21 10:38:46 -0700

    [ UserDefinedException.java ]add: Welcome

---
+++ corejava/UserDefinedException.java
@@ -0,0 +1,9 @@
+public class UserDefinedException extends Exception {
+	
+    public String errormessage;
+    
+    //Initializing Constructor
+	public UserDefinedException(String message) {
+		this.errormessage = message;	
+	}
+}
