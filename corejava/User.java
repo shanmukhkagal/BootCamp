f7015fde3785d531b5cede1cc48e29000171ad9e
shanmukhkagal <kagalshanmukh@gmail.com>
2021-04-25 20:41:41 -0700

    [ User.java ]add: Welcome

---
+++ corejava/User.java
@@ -0,0 +1,9 @@
+public interface user {
+    /**
+     * lambda function created to validate pattern
+     * @param pattern
+     * @param input
+     * @return
+     */
+    void validateUser(String pattern, String input);
+}
